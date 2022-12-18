11911291aac91080db34fafbf1a3cd991ef728bd
Zach <zacharyzwilson@outlook.com>
2022-12-18 15:14:58 -0600

    last commit, I hope

--- calc/app.py
+++ calc/app.py
@@ -7,36 +7,55 @@ app = Flask(__name__)
 
 
 @app.route("/add")
-def add():
+def add_func():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     result = add(a, b)
 
-    return result
+    return str(result)
 
 
 @app.route("/sub")
-def sub():
+def sub_func():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     result = sub(a, b)
 
-    return result
+    return str(result)
 
 
 @app.route("/mult")
-def mult():
+def mult_func():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     result = mult(a, b)
 
-    return result
+    return str(result)
 
 
 @app.route("/div")
-def div():
+def div_func():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     result = div(a, b)
 
-    return result
+    return str(result)
+
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+
+@app.route("/math/<oper>")
+def all_math_func(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
